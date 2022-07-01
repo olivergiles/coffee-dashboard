688fde9a5633855f480b1fee951573bfec122432
Oliver Giles <ogiles@email.com>
2022-07-01 20:06:11 +0200

    testing db access in body of app

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
+import pandas as pd
 import streamlit as st
+
+from app.database import CoffeeDB
 
 class Dashboard():
     def __init__(self) -> None:
-        pass
+        self.db = CoffeeDB()
 
     def title(self) -> None:
         st.title("Coffee Quality Dashboard!")
 
+    def body(self) -> None:
+        cursor = self.db.get_cursor()
+        test_query = pd.read_sql_query("SELECT * FROM merged_data_cleaned", cursor, index_col='')
+        st.dataframe(test_query)
+
     def render(self) -> None:
         self.title()
+        self.body()
 
 if __name__ == "__main__":
     dashboard = Dashboard()
