0adf8ae58e6579a02274ff05acfd2f0ee92bba88
Oliver Giles <ogiles@email.com>
2022-07-11 13:58:52 +0200

    updated doc strings for cache

--- app/cache.py
+++ app/cache.py
@@ -1,25 +1,29 @@
+from typing import Any
 import streamlit as st
 
 __all__ = ["Cache"]
 
 
 class Cache:
-    def cache_key_value(self, key, value):
-        """_summary_
+    """Class for the streamlit cache object.
+    """
+    def cache_key_value(self, key: str, value: Any) -> None:
+        """Function to cache a value in streamlit cache.
 
         Args:
-            key (_type_): _description_
-            value (_type_): _description_
+            key (str): Key to cache value.
+            value (Any): Value to cache.
         """
         st.session_state[key] = value
 
-    def get_value_for_key(self, key):
-        """_summary_
+    def get_value_for_key(self, key: str) -> Any:
+        """Get a value for a given key from streamlit cache.
 
         Args:
-            key (_type_): _description_
+            key (str): Key to cache value.
 
         Returns:
-            _type_: _description_
+            Any: The value for the given key from the
+            cache or None if not in cache.
         """
         return st.session_state.get(key, None)
